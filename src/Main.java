520fa489135141a477de4d5943732f2ca4a12afb
Mauro <enanobrillante@gmail.com>
2025-11-09 10:23:12 +0100

    Implementada funcionalidad de login

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Versión inicial del proyecto.");
+        System.out.println("Funcionalidad de login implementada.");
     }
 }
